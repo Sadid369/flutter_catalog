8f92dbac304d34abf20f3877d332585071cdd874
Sadid369 <sadid.jones@gmail.com>
2021-05-22 07:11:08 +0600

    Day Three Completed

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,7 @@ class MyApp extends StatelessWidget {
       ),
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
+      initialRoute: '/home',
       routes: {
         '/': (context) => LoginPage(),
         '/home': (context) => HomePage(),
