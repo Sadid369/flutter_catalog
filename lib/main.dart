1d7c1cf6241056c1a4aa556cdcee0fa6e0e87bc5
Sadid369 <sadid.jones@gmail.com>
2021-05-22 07:08:43 +0600

    Day 3 Completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
+import 'pages/home_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,7 +13,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     int days = 30;
     return MaterialApp(
-      home: HomePage(),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      routes: {
+        '/': (context) => LoginPage(),
+        '/home': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
